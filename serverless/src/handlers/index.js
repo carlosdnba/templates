a100647f6b9c4ffa120fe3c3861378c16be2162c
Carlos Daniel <carlos.daniel@devsquad.email>
2021-07-03 17:23:23 -0300

    fix: removing nonexistent files

--- serverless/src/handlers/index.js
+++ serverless/src/handlers/index.js
@@ -1,5 +1,3 @@
-import { firstGet } from './first-get';
 import { healthCheck } from './health-check';
-import { newChapter } from './new-chapter';
 
-export { firstGet, healthCheck, newChapter };
+export { healthCheck };
